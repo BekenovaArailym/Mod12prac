1b0bc9ed8ae89c4c927b777be20eaebbaad9b4a0
Arailym Bekenova <030204600628@stud.satbayev.university>
2023-11-26 23:25:03 +0600

    Добавьте файлы проекта.

---
+++ MyClass.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ClassLib
+{
+    public class MyClass : IPropertyChanged
+    {
+        private string myProperty;
+
+        public string MyProperty
+        {
+            get { return myProperty; }
+            set
+            {
+                if (myProperty != value)
+                {
+                    myProperty = value;
+                    NotifyPropertyChanged(nameof(MyProperty));
+                }
+            }
+        }
+
+        public event PropertyEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyEventArgs(propertyName));
+        }
+    }
+
+    public interface IPropertyChanged
+    {
+        event PropertyEventHandler PropertyChanged;
+    }
+
+    public delegate void PropertyEventHandler(object sender, PropertyEventArgs e);
+
+    public class PropertyEventArgs : EventArgs
+    {
+        public string PropertyName { get; }
+
+        public PropertyEventArgs(string propertyName)
+        {
+            PropertyName = propertyName;
+        }
+    }
+}
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ClassLib
+{
+    public class MyClass
+    {
+        private string myProperty;
+
+        public event EventHandler<PropertyEventArgs> PropertyChanged;
+
+        public string MyProperty
+        {
+            get { return myProperty; }
+            set
+            {
+                if (myProperty != value)
+                {
+                    myProperty = value;
+                    OnMyPropertyChanged(nameof(MyProperty));
+                }
+            }
+        }
+
+        protected virtual void OnMyPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyEventArgs(propertyName));
+        }
+    }
+
+    public class PropertyEventArgs : EventArgs
+    {
+        public string PropertyName { get; }
+
+        public PropertyEventArgs(string propertyName)
+        {
+            PropertyName = propertyName;
+        }
+    }
+}
